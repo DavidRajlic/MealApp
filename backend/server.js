0484b1d20b46da05f1f12eb13e3c09bbf2126c3f
DavidRajlic <rajlic.david@gmail.com>
2025-04-23 15:07:20 +0200

    created user model and crud user functions

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv').config(); // Za nalaganje .env datoteke
 
 const app = express();
-const port = 3001;
+const port = 4001;
 
 app.use(express.json());
 
