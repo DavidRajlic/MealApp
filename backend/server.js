fd9d5653412b9a03a2cc557a3568ca37ef7a919a
DavidRajlic <rajlic.david@gmail.com>
2025-05-12 22:57:35 +0200

    port change

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config(); // Za nalaganje .env datoteke
+require('dotenv').config(); 
 const userRoutes = require('./routes/UserRoutes');
 const restaurantRoutes = require('./routes/RestaurantRoutes');
 const reviewRoutes = require('./routes/ReviewRoutes');
 
 const app = express();
-const port = 4001;
+const port = 4000;
 
 app.use(express.json());
 
