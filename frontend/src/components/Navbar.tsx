bc0e51843706450899856e947f9f0ff70a957fb2
DavidRajlic <rajlic.david@gmail.com>
2025-05-30 01:57:01 +0200

    added profile link

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,50 +1,63 @@
 // src/components/Navbar.tsx
 import { Link } from 'react-router-dom'
-import {useAuth} from '../contexts/AuthContext.ts'
+import { useAuth } from '../contexts/AuthContext.ts'
 
 const Navbar = () => {
     const { isLoggedIn, user, logout } = useAuth();
 
-    
-  return (
-    <nav className="bg-white p-4 shadow flex items-center">
-  <span className="text-2xl font-bold text-[#c45a39]" > <Link to="/"> MealApp </Link>  </span> 
 
-  <div className="ml-auto flex items-center gap-6">
-    
-      {isLoggedIn ? (
-          <Link
-      to="/"
-      className="text-[#c45a39] hover:text-blue-500 transition"
-    > <span onClick={logout}>  Odjava</span>
-     </Link>
-        
-      ) : (
-       <Link
-      to="/login"
-      className="text-[#c45a39] hover:text-blue-500 transition"
-    > <span> Prijava</span>
-     </Link>
-      )}
-   
+    return (
+        <nav className="bg-white p-4 shadow flex items-center">
+            <span className="text-2xl font-bold text-[#c45a39]" > <Link to="/"> MealApp </Link>  </span>
 
-    <Link
-      to="/register"
-      className="text-[#c45a39] hover:text-blue-500 transition"
-    >
-      Registracija
-    </Link>
+            <div className="ml-auto flex items-center gap-6">
 
-    <Link
-      to="/restaurants"
-      className="text-[#c45a39] hover:text-blue-500 transition"
-    >
-      Lokali
-    </Link>
-  </div>
-</nav>
+                 {
+                    isLoggedIn && (<Link
+                        to="/restaurants"
+                        className="text-[#c45a39] hover:text-blue-500 transition"
+                    >
+                        Lokali
+                    </Link>)
+                }
 
-  )
+                {
+                    isLoggedIn && (<Link
+                        to="/profile"
+                        className="text-[#c45a39] hover:text-blue-500 transition"
+                    >
+                        Profil
+                    </Link>)
+                }
+
+                {isLoggedIn ? (
+                    <Link
+                        to="/"
+                        className="hover:text-blue-500 transition"
+                    > <span onClick={logout}>  Odjava</span>
+                    </Link>
+
+                ) : (
+                    <Link
+                        to="/login"
+                        className="text-[#c45a39] hover:text-blue-500 transition"
+                    > <span> Prijava</span>
+                    </Link>
+                )}
+
+                {
+                    !isLoggedIn && (<Link
+                        to="/register"
+                        className="text-[#c45a39] hover:text-blue-500 transition"
+                    >
+                        Registracija
+                    </Link>)
+                }
+
+            </div>
+        </nav>
+
+    )
 }
 
 export default Navbar
