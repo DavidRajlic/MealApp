84b2e68034f91e4e9c9d8a482d64bd2bb1e4d2bd
DavidRajlic <55549698+DavidRajlic@users.noreply.github.com>
2025-05-30 01:07:35 +0200

    Update Login.tsx

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,10 +1,13 @@
 import { type FormEvent, useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [statusMessage, setStatusMessage] = useState("");
   const API_URL = import.meta.env.VITE_API_URL;
+
+  const navigate = useNavigate();
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
@@ -20,7 +23,12 @@ const Login = () => {
 
       if (res.ok) {
         const data = await res.json();
-        setStatusMessage(`✅ Prijava uspešna! Dobrodošel, ${data.username || email}`);
+
+        localStorage.setItem('token', data.token); 
+        localStorage.setItem('user', JSON.stringify(data.user)); 
+
+        setStatusMessage(`✅ Prijava uspešna! Dobrodošel, ${data.user.username || email}`);
+        navigate(`/restaurants`);
       } else {
         const error = await res.json();
         setStatusMessage(`❌ Napaka: ${error.message || "Napačni podatki"}`);
@@ -33,7 +41,8 @@ const Login = () => {
   return (
     // Outer cream-colored full-screen wrapper
     <div className="min-h-screen flex items-center justify-center bg-[#fffaf0] p-4">
-      <div className="max-w-2xl mx-auto bg-white p-8 rounded-xl shadow-lg border border-gray-100 mt-12">
+      <div className="max-w-2xl w-full  bg-white p-8 rounded-xl shadow-lg border border-gray-100 mt-12">
+
         {/* Flex wrapper: form on left, image on right */}
         <div className="flex flex-col md:flex-row items-center md:items-start gap-8">
           
@@ -88,15 +97,6 @@ const Login = () => {
               </p>
             )}
           </form>
-
-          {/* Right: decorative/login illustration */}
-          <div className="hidden md:block md:flex-shrink-0">
-            <img
-              src="/images/login.png"
-              alt="Login illustration"
-              className="w-40 h-40 object-cover rounded-full shadow-md"
-            />
-          </div>
         </div>
       </div>
     </div>
