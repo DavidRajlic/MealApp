b31bd1dae8a33eaa47ff88535412bbf8be3981f4
DavidRajlic <rajlic.david@gmail.com>
2025-06-11 19:40:21 +0200

    change api url and send auth header when posting review

--- frontend/src/pages/Restaurant.tsx
+++ frontend/src/pages/Restaurant.tsx
@@ -59,9 +59,10 @@ const Restaurant = () => {
     setSubmitting(true);
 
     try {
-      const res = await fetch(`http://localhost:4000/reviews`, {
+      const res = await fetch(`${API_URL}/reviews`, {
         method: 'POST',
         headers: {
+          'Authorization': 'Bearer ' + localStorage.getItem('token'),
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
