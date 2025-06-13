91d05f8bf0716a36725fa86996f157708206f46c
DavidRajlic <rajlic.david@gmail.com>
2025-06-13 16:58:05 +0200

    removed memeber from about us page

--- frontend/src/pages/AboutUs.tsx
+++ frontend/src/pages/AboutUs.tsx
@@ -28,13 +28,6 @@ const team = [
     gender: "male",
   },
   {
-    name: "Klemen Poglajen",
-    role: "Backend razvijalec",
-    description:
-      "Klemen pomaga pri strežniških opravilih, piše poročila in dokumentacijo.",
-    gender: "male",
-  },
-  {
     name: "Tatjana Panova, Rebeka Nikolova in Ana Klincharova",
     role: "UI oblikovalke",
     description:
